2d1823193d78605aeddcd2348cc7e4f62f053638
sergey freeman <sergeyrar@gamil.com>
2017-11-18 14:29:52 +0200

    moved thread code to main

--- server/comms.c
+++ server/comms.c
@@ -8,77 +8,16 @@
 #include <sys/types.h>
 #include <unistd.h>  
 #include <errno.h>
-#include <pthread.h>
-#include <assert.h>
+
 
 #include "comms.h"
 #include "error.h"
-
-/* Local Functions */
-void * rcv_from_clients_thread(void * chat_conn_ptr)
-{
-	int len;
-	connection_t *chat_conn = (connection_t *)chat_conn_ptr;
-	
-	while (1)
-	{
-		for (int i = 0; i < CLIENT_MAX; i++)
-		{
-			if (chat_conn->connfd[i] != 0)
-			{	//should probably be NON-blocking recv!
-				if ((len = recv(chat_conn->connfd[i], chat_conn->chat_buff.data, BUF_MAX, 0)) < 0)
-				{
-					perror("recv failed");
-					return NULL;
-				}
-				
-				chat_conn->chat_buff.buf_len += len;
-			}
-		}
-		
-	}
-	
-	assert(0);
-	return NULL;
-
-}
-
-
-
-void * send_to_clients_thread(void * chat_conn_ptr)
-{
-	int len;
-	connection_t *chat_conn = (connection_t *)chat_conn_ptr;
-	
-	while (1)
-	{
-		for (int i = 0; i < CLIENT_MAX; i++)
-		{
-			if (chat_conn->connfd[i] != 0)
-			{
-				if ((len = send(chat_conn->connfd[i], chat_conn->chat_buff.data, chat_conn->chat_buff.buf_len, 0)) < 0)
-				{
-					perror("send failed");
-					return NULL;
-				}
-			}
-		}
-		
-		chat_conn->chat_buff.buf_len = 0;
-	}
-	
-	assert(0);
-	return NULL;
-}
-
 
 
 
 int server_start(connection_t *chat_conn)
 {
 	int i=0;
-	pthread_t recv_thread;
-	pthread_t send_thread;
 	
 	chat_conn->listenfd = socket(AF_INET, SOCK_STREAM, 0); 
     chat_conn->serv_addr.sin_family = AF_INET;
@@ -87,16 +26,6 @@ int server_start(connection_t *chat_conn)
     
     bind(chat_conn->listenfd, (struct sockaddr*)&chat_conn->serv_addr, sizeof(chat_conn->serv_addr)); 
     listen(chat_conn->listenfd, CLIENT_MAX); 
-    
-	if (pthread_create(&send_thread, NULL, send_to_clients_thread, chat_conn) != 0)
-	{
-		perror("send pthread creation failed\n");
-	}
-		
-	if (pthread_create(&recv_thread, NULL, rcv_from_clients_thread, chat_conn) != 0)
-	{
-		perror("rcv pthread creation failed\n");
-	}
     
 	while(1)
     {
--- server/server.c
+++ server/server.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <pthread.h>
+#include <assert.h>
+
 
 #include "comms.h"
 #include "error.h"
@@ -8,12 +11,82 @@
 
 
 
+void * rcv_from_clients_thread(void * chat_conn_ptr)
+{
+	int len;
+	connection_t *chat_conn = (connection_t *)chat_conn_ptr;
+	
+	while (1)
+	{
+		for (int i = 0; i < CLIENT_MAX; i++)
+		{
+			if (chat_conn->connfd[i] != 0)
+			{	//should probably be NON-blocking recv!
+				if ((len = recv(chat_conn->connfd[i], chat_conn->chat_buff.data, BUF_MAX, 0)) < 0)
+				{
+					perror("recv failed");
+					return NULL;
+				}
+				
+				chat_conn->chat_buff.buf_len += len;
+			}
+		}
+		
+	}
+	
+	assert(0);
+	return NULL;
+
+}
+
+
+void * send_to_clients_thread(void * chat_conn_ptr)
+{
+	int len;
+	connection_t *chat_conn = (connection_t *)chat_conn_ptr;
+	
+	while (1)
+	{
+		for (int i = 0; i < CLIENT_MAX; i++)
+		{
+			if (chat_conn->connfd[i] != 0)
+			{
+				if ((len = send(chat_conn->connfd[i], chat_conn->chat_buff.data, chat_conn->chat_buff.buf_len, 0)) < 0)
+				{
+					perror("send failed");
+					return NULL;
+				}
+			}
+		}
+		
+		chat_conn->chat_buff.buf_len = 0;
+	}
+	
+	assert(0);
+	return NULL;
+}
+
 
 int main(int argc, char *argv[])
 {
 	int rv = OK;
+	pthread_t recv_thread;
+	pthread_t send_thread;
+	
 	connection_t chat_conn;
 	memset(&chat_conn, 0, sizeof(connection_t));
+	
+	if (pthread_create(&send_thread, NULL, send_to_clients_thread, &chat_conn) != 0)
+	{
+		perror("send pthread creation failed\n");
+	}
+		
+		
+	if (pthread_create(&recv_thread, NULL, rcv_from_clients_thread, &chat_conn) != 0)
+	{
+		perror("rcv pthread creation failed\n");
+	}
+	
 	
 	if ((rv = server_start(&chat_conn)) != OK)
 	{
