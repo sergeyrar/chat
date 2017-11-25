6e5142a51cc9f9a6cb2439b549978271cfd6b703
sergey freeman <sergeyrar@gamil.com>
2017-11-25 15:04:40 +0200

    Server now running as daemon

--- server/server.c
+++ server/server.c
@@ -4,6 +4,11 @@
 #include <pthread.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 
 #include "comms.h"
@@ -75,26 +80,43 @@ void * send_to_clients_thread(void * chat_conn_ptr)
 }
 
 
+static void daemon_fd_init()
+{
+	int fd = open("./server.log", O_WRONLY);
+	
+	if (fd < 0) perror("Failed to open file for writing");
+	
+	dup2(fd, 1);
+	dup2(fd, 2);
+	
+	close(fd);
+}
+
+
 int main(int argc, char *argv[])
 {
 	int rv = OK;
 	pthread_t recv_thread;
 	pthread_t send_thread;
-	
 	connection_t chat_conn;
+	pid_t pid;
+	
+	pid = fork();
+	// check for fork error
+	if (pid < 0) perror("Fork failed\n");
+	//exit from parent process
+	if (pid > 0) exit(0);
+	
+	// Now running as daemon in child process
+	daemon_fd_init();
+
 	memset(&chat_conn, 0, sizeof(connection_t));
 	
 	if (pthread_create(&send_thread, NULL, send_to_clients_thread, &chat_conn) != 0)
-	{
-		perror("send pthread creation failed\n");
-	}
-		
+		perror("send pthread creation failed\n");	
 		
 	if (pthread_create(&recv_thread, NULL, rcv_from_clients_thread, &chat_conn) != 0)
-	{
 		perror("rcv pthread creation failed\n");
-	}
-	
 	
 	if ((rv = server_start(&chat_conn)) != OK)
 	{
